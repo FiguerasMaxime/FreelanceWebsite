77105722d85e11db94b3eba30a213edea6d8ea4a
FiguerasMaxime <maxime.figueras@gmail.com>
2019-12-02 01:08:58 +0100

    2nd commit : Page Projects Built, more animations, interactions social media

---
+++ js/parallax.js
@@ -0,0 +1,33 @@
+ScrollOut({
+    threshold: .6
+})
+
+var rellax = new Rellax('.rellax');
+
+
+// window.addEventListener('scroll', ()=>{
+//     function offset(elt) {
+//         var rect = elt.getBoundingClientRect(), bodyElt = document.body;
+//         return {
+//             top: rect.top + bodyElt .scrollTop,
+//             left: rect.left + bodyElt .scrollLeft
+//         }
+//     }
+//     var offsetElt = offset(document.getElementById('isoMessage'));
+//     offsetElt.top;
+//     offsetElt.left;
+
+//     var topSvg = offsetElt.top; // affiche la différence de hauteur entre le haut du svg et le haut 
+//     var topScreen = window.scrollY; //affiche la hauteur par rapport à 0 de base
+//     console.log(topScreen); 
+//     console.log(topSvg);
+
+//     var decalage = 400;
+//     var topBetween = topSvg - 400;
+
+//     console.log(topBetween)
+
+
+// })
+
+
